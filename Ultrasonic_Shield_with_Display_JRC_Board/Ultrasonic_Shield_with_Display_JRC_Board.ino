bf4bc934592bf491cfbbe1a32f69c9877eefdeb2
fazleyelahi <fazleyelehi@gmail.com>
2023-06-07 18:27:49 +0600

    .

--- Ultrasonic_Shield_with_Display_JRC_Board/Ultrasonic_Shield_with_Display_JRC_Board.ino
+++ Ultrasonic_Shield_with_Display_JRC_Board/Ultrasonic_Shield_with_Display_JRC_Board.ino
@@ -1,27 +1,28 @@
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
-LiquidCrystal_I2C lcd(0x27, 16, 2);
-int trig = 19, echo = 18;
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(5, 14, 15, 16, 17, 4);
+const int trig = 19, echo = 18, buzzer = 26;
+
 void setup() {
   pinMode(trig, OUTPUT);
   pinMode(echo, INPUT);
-  // pinMode(4, OUTPUT);
-  // digitalWrite(4, 0);
-  Serial.begin(9600);
-  lcd.init();
-  lcd.backlight();
+  pinMode(buzzer, OUTPUT);
+  Serial.begin(115200);
+  lcd.begin(16, 2);
 }
+
 void loop() {
   lcd.setCursor(0, 0);
   lcd.print("Distance:");
-  int x = reading();
+  int x = read2ing();
   lcd.print(String(x));
   Serial.println("Distance: " + String(x) + "cm");
   lcd.print("cm   ");
   lcd.setCursor(0, 1);
   (x < 5 && x != 0) ? lcd.print("   WARNING!!!   ") : lcd.print("                ");
+  (x < 5 && x != 0) ? digitalWrite(buzzer, 1) : digitalWrite(buzzer, 0);
   delay(100);
 }
+
 int reading() {
   digitalWrite(trig, LOW);
   delayMicroseconds(2);
