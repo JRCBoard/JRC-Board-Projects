da9cb2ac0435146de96fc1d190ef3ff57a360335
fazleyelahi <fazleyelehi@gmail.com>
2023-06-08 20:21:33 +0600

    button rearrangement

--- snake_game/game_over.ino
+++ snake_game/game_over.ino
@@ -7,7 +7,7 @@ int game_over(int a) {
   }
   value = String(a);
   int menu = 0;
-  while (!digitalRead(sw[1]) && !digitalRead(sw[3])) {
+  while (digitalRead(sw[1]) && digitalRead(sw[3])) {
     display.clearDisplay();
     display.setTextSize(2);
     display.setTextColor(WHITE);
@@ -18,10 +18,10 @@ int game_over(int a) {
     text("              EXIT", 04, 55);
     (menu == 0) ? text(">          <", 04, 55) : text("             >    <", 04, 55);
     display.display();
-    if (digitalRead(sw[0])) menu = 1;
-    else if (digitalRead(sw[2])) menu = 0;
+    if (!digitalRead(sw[0])) menu = 1;
+    else if (!digitalRead(sw[2])) menu = 0;
   }
-  while (digitalRead(sw[1]) || digitalRead(sw[3]));
+  while (!digitalRead(sw[1]) || !digitalRead(sw[3]));
   display.setTextSize(2);
   return menu;
 }
